7a8a3a0d94687d343a308ab4fd98c54560374d36
Sean McDermott <seanjmac@hotmail.com>
2018-07-12 17:46:54 -0700

    Add album list component and link to App

--- App.js
+++ App.js
@@ -9,6 +9,7 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // const instructions = Platform.select({
 //   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,7 +22,10 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <Header headerText={'Albums'} />
+      <View>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+      </View>
     );
   }
 }
