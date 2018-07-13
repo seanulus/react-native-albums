d4ef6359f3695f189f6835426af0c0ff90ec194c
Sean McDermott <seanjmac@hotmail.com>
2018-07-12 17:00:41 -0700

    Add initial project. Add header component and basic styling

---
+++ index.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as albums } from './app.json';
+
+AppRegistry.registerComponent(albums, () => App);
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+const Header = () => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8'
+  },
+  textStyle: {
+    fontSize: 20
+  }
+};
+
+export default Header;
